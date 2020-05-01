7ac7cff3c3b1f63d0533457b87bacfbf7b6e4ce8
Fernando Bahamondes <ferbaco@gmail.com>
2020-05-01 18:15:50 -0400

    Fix rubocop offenses

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,12 @@
 class Player
-  @@win_conditions = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+  @win_conditions = [[1, 2, 3],
+                     [4, 5, 6],
+                     [7, 8, 9],
+                     [1, 4, 7],
+                     [2, 5, 8],
+                     [3, 6, 9],
+                     [1, 5, 9],
+                     [3, 5, 7]]
 
   attr_reader :pos
   attr_reader :name
@@ -17,7 +24,7 @@ class Player
   end
 
   def check_winner
-    @@win_conditions.each do |arr|
+    @win_conditions.each do |arr|
       cont = 0
       arr.each do |item|
         cont += 1 if @pos.include?(item.to_s)
