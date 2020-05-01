fdf0f3c8533ce2d6bd77ccb28c0e58aef2b0d55e
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-05-01 17:49:46 -0500

    Corrected more Rubocop offenses

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,4 @@
 class Player
-
   attr_reader :pos
   attr_reader :name
   attr_reader :symbol
@@ -9,13 +8,13 @@ class Player
     @symbol = symbol
     @pos = []
     @win_conditions = [[1, 2, 3],
-                     [4, 5, 6],
-                     [7, 8, 9],
-                     [1, 4, 7],
-                     [2, 5, 8],
-                     [3, 6, 9],
-                     [1, 5, 9],
-                     [3, 5, 7]]
+                       [4, 5, 6],
+                       [7, 8, 9],
+                       [1, 4, 7],
+                       [2, 5, 8],
+                       [3, 6, 9],
+                       [1, 5, 9],
+                       [3, 5, 7]]
   end
 
   def add_pos(pos)
