2a3545f519daa48e0eefd93658cc9a41c5b0f1be
Fernando Bahamondes <ferbaco@gmail.com>
2020-05-05 11:44:46 -0400

    Added more tests for the GameLogic class. round and game_over methods

--- spec/gamelogic_spec.rb
+++ spec/gamelogic_spec.rb
@@ -29,6 +29,27 @@ describe GameLogic do
   end
 
   describe '#initialize_board' do
+    it 'Returns the clean drawn board' do
+
+      expect(game.initialize_board).to eql(["    |   |    \n  1 | 2 | 3  \n____|___|____", "    |   |    \n  4 | 5 | 6  \n____|___|____", "    |   |    \n  7 | 8 | 9  \n____|___|____"])
+    end
+  end
+
+  describe '#validate' do
+    it 'Returns true for the position 3' do
+      expect(game.validate(3)).to eql(true)
+    end
+
+    it 'Returns false for input greater than 9' do
+      expect(game.validate(15)).to eql(false)
+    end
+
+    it 'Returns false for input different than number' do
+      expect(game.validate('q')).to eql(false)
+    end
+  end
+
+  describe '#round' do
     it 'Returns the drawn board for player1 with positions 1, 5, 8 and player2 positions 4, 7, 9' do
       player1.add_pos('1')
       player1.add_pos('5')
@@ -36,15 +57,14 @@ describe GameLogic do
 
       player2.add_pos('4')
       player2.add_pos('7')
-      player2.add_pos('9')
 
-      expect(game.initialize_board).to eql(["    |   |    \n  X | 2 | 3  \n____|___|____", "    |   |    \n  O | X | 6  \n____|___|____", "    |   |    \n  O | X | O  \n____|___|____"])
+      expect(game.round(5, '9')).to eql(["    |   |    \n  X | 2 | 3  \n____|___|____", "    |   |    \n  O | X | 6  \n____|___|____", "    |   |    \n  O | X | O  \n____|___|____"])
     end
   end
 
-  describe '#validate' do
-    it 'Returns true for the position 3' do
-      expect(game.validate(3)).to eql(true)
+  describe '#game_over' do
+    it 'Returns true when game is finished' do
+      expect(game.game_over).to eql(true)
     end
   end
 end
